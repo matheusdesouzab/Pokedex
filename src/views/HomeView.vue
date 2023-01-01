93b3b262bbc26c4c01b2f8e5e43056af7b2599b3
Matheus de Souza <matheusdesouza187@gmail.com>
2023-01-01 20:17:34 -0300

    feat: Remoção dos arquivos desnecessarios no projeto, e incorporação do Boostrap no projeto

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,15 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row mt-2">
+      <div class="col mb-2">PALCO</div>
+      <div class="col mb-2">POKDEX</div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  name: 'HomeView'
 }
 </script>
